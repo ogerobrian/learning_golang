992bcc9b70b8749da23d694763d52d8e5ef1e082
ogerobrian <omwengabriankaka@gmail.com>
2021-08-09 00:16:07 +0300

    Another commit

--- embbed_field.go
+++ embbed_field.go
@@ -1,9 +1,33 @@
 package main
 
+import "fmt"
+
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+type Student struct {
+	Human
+	specialty string
+}
+
 func main() {
-	type Human struct {
-		name   string
-		age    int
-		weight int
-	}
+	mark := Student{Human{"Mark", 25, 120}, "Computer Science"}
+
+	fmt.Println("His name is ", mark.name)
+	fmt.Println("His age is ", mark.age)
+	fmt.Println("His weight is ", mark.weight)
+	fmt.Println("His specialty is ", mark.specialty)
+
+	mark.specialty = "AI"
+	fmt.Println("Mark changed his specialty")
+	fmt.Println("His specialty is ", mark.specialty)
+
+	fmt.Println("Mark become old. He is not an athlete anymore")
+	mark.age = 46
+	mark.weight += 60
+	fmt.Println("His age is", mark.age)
+	fmt.Println("His weight is", mark.weight)
 }
