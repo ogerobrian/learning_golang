428bff76ef0719a4a13c6e03f644b91d9aa5af09
ogerobrian <omwengabriankaka@gmail.com>
2021-08-08 23:12:26 +0300

    another commmit

---
+++ arry.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [...]int{}             // empty array
+	arr2 := [5]int{1, 2, 3, 4, 5} // array of length 5
+	fmt.Println(arr)
+	fmt.Println(arr2)
+}
